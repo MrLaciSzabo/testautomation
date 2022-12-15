3f932f121f53a46235f7b333c57a4d2dfa99cbe7
Szabó Laci <mr.laci.szabo@gmail.com>
2022-12-15 14:38:51 +0100

    RegPage1

--- src/test/java/pages/RegPage1.java
+++ src/test/java/pages/RegPage1.java
@@ -1,15 +1,37 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+
+import java.util.UUID;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.Assert.assertEquals;
 public class RegPage1 {
 
     // UI mezők
-    SelenideElement email = ${}; //
+    //SelenideElement email = ${};
+    SelenideElement stageNumber = $(".progress-bar__stage-number");
+    SelenideElement regSubBtn = $(byText("Next"));
 
-    public void register() {
-        email.set("..."); // email generator (Random string generalas)
+    public RegPage2 setRegProcess1() {
+        SelenideElement inputEmail = $("#email");
+        SelenideElement inputPass = $("#password");
+        SelenideElement inputPassConf = $("#confirm-password");
 
-        // next.click();
+        assertEquals("1", stageNumber.getText());
+        UUID emailUUID = UUID.randomUUID();
+        String randMail = emailUUID + "@test.it";
+
+        inputEmail.setValue(randMail);
+        inputPass.setValue("Q1w2e3r4");
+        inputPassConf.setValue("Q1w2e3r4");
+        regSubBtn.click();
         return new RegPage2();
+
+
+
+
     }
 
 }
