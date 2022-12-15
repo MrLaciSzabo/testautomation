7c34d244c865b6604e3ac4da895700fafcc48c4e
Szabó Laci <mr.laci.szabo@gmail.com>
2022-12-15 10:22:00 +0100

    Steps

--- src/test/java/steps/TescoWebsite.java
+++ src/test/java/steps/TescoWebsite.java
@@ -1,17 +1,23 @@
 package steps;
 
+import com.codeborne.selenide.Configuration;
 import io.cucumber.java.Before;
 import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.MainPage;
+import pages.RegPage1;
+import pages.RegPage2;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,13 +28,22 @@ public class TescoWebsite {
     private WebDriverWait wait;
 
     @Before
+    //kikerülöm a automaták tiltását
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        WebDriverManager.chromedriver().setup();
+        Configuration.browserCapabilities = options;
+
+        System.out.println("setup code");
+    }
+
+
     public void MainPage() {
-        Given("^homepage is opened$", () -> {
-        });
-        And("^cookies are set$", () -> {
-        });
-        When("^user registers$", () -> {
-        });
+        MainPage mainPage = new mainPage();
+        mainPage.acceptCookies();
+
+
     }
         public void user_registers() {
         RegPage1 regPage1 = open(...);
