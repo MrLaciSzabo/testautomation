76f2209f42353108494b3bb7bbf639e6180a4103
Szabó Laci <mr.laci.szabo@gmail.com>
2022-12-16 09:04:54 +0100

    ConfirmPage

--- src/test/java/pages/ConfirmPage.java
+++ src/test/java/pages/ConfirmPage.java
@@ -1,4 +1,14 @@
 package pages;
 
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.Assert.assertTrue;
+
 public class ConfirmPage {
+    public MainPage setConfirmReg() {
+        return null;
+    }
+
+    public void confirmReg() {
+        assertTrue($("#utility-header-orders-link").isDisplayed());
+    }
 }
