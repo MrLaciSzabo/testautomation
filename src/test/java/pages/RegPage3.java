e17d6543f1b44599930c5662970fc607f596447c
Szabó Laci <mr.laci.szabo@gmail.com>
2022-12-16 09:06:45 +0100

    RegPage3

--- src/test/java/pages/RegPage3.java
+++ src/test/java/pages/RegPage3.java
@@ -1,4 +1,33 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+
+import javax.security.auth.callback.ConfirmationCallback;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
 public class RegPage3 {
+        public ConfirmPage setRegProcess3() {
+
+        SelenideElement firstName = $("#firstName");
+        SelenideElement lastName = $("#lastName");
+        SelenideElement primaryPhone = $("#primaryPhone");
+        SelenideElement consentCheckBox = $(byXpath("//*[@id=\"personalDetails\"]/ul[4]/li/div/label"));
+        SelenideElement regFinalizeBtn = $(byText("Yes, register me"));
+
+        firstName.setValue("Teszt");
+        lastName.click();
+        lastName.sendKeys("User");
+        primaryPhone.setValue("06123456789");
+
+        consentCheckBox.click();
+        regFinalizeBtn.click();
+        return new ConfirmPage() {
+            @Override
+            public MainPage setRegConfirm() {
+                return null;
+            }
+        };
+    }
 }
