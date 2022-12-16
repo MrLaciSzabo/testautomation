27e3036f8b5a11545a45281160758038ddc93f86
Szabó Laci <mr.laci.szabo@gmail.com>
2022-12-16 09:08:15 +0100

    TescoWebsite

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,8 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features",
+        publish = true,
         glue = "steps",
-        plugin = {"pretty", "json:target/cucumber-reports.json",
+        plugin = {"pretty",
+                "json:target/cucumber-reports.json",
                 "junit:target/cucumber-reports.xml",
                 "html:target/cucumber-reports.html",
                 "rerun:target/cucumber-rerun.txt"})
